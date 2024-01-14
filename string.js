f4d9daf4570c891a68f8812c9216e6ef5fc1b6da
Arjun cv <arjun7180@gmail.com>
2024-01-14 23:19:13 +0530

    1

---
+++ string.js
@@ -0,0 +1,18 @@
+//344. Reverse String
+var reverseString = function (s) {
+
+    let start = 0;
+    let end = s.length - 1;
+
+    while (start < end) {
+
+        let temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    return s;
+};
