1788628c699632bab6516376dcf3d9e9326925b4
Arjun cv <arjun7180@gmail.com>
2024-02-12 14:27:06 +0530

    Easy

--- Problems.js
+++ Problems.js
@@ -899,29 +899,6 @@ var rotate = function (nums, k) {
     return nums;
 };
 
-var maxProfit = function (prices) {
-
-    let profit = 0;
-
-    for (let i = 0; i < prices.length - 1; i++) {
-
-        for (let j = i + 1; j < prices.length; j++) {
-
-            if (prices[i] < prices[j] + 1) {
-
-                profit += prices[j] + 1 - prices[i];
-
-                i = j;
-
-                break;
-            }
-
-        }
-    }
-
-    return profit;
-};
-
 //Medium
 //122. Best Time to Buy and Sell Stock II
 var maxProfit = function (prices) {
