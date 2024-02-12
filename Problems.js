d00319d4dc26d67f65eaa65a50bb19d4bbec347b
Arjun cv <arjun7180@gmail.com>
2024-02-12 16:43:44 +0530

    easy

--- Problems.js
+++ Problems.js
@@ -923,3 +923,26 @@ var lengthOfLastWord = function (s) {
 
     return words.at(-1).trim().length;
 };
+
+//125. Valid Palindrome
+var isPalindrome = function (s) {
+
+    let letters = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+    return isPal(letters);
+
+    function isPal(s) {
+        let start = 0;
+        let end = s.length - 1;
+
+        while (start < end) {
+            if (s[start] !== s[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+};
