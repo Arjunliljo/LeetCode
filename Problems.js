4ab336bdcb14686b407137d10551a8e93e03b3b9
Arjun cv <arjun7180@gmail.com>
2024-02-26 22:42:56 +0530

    Medium

--- Problems.js
+++ Problems.js
@@ -1189,4 +1189,37 @@ var reverse = function (x) {
 
     return x < 0 ? -ans : ans;
 };
+//Medium
+//15. 3Sum
+var threeSum = function (nums) {
+    const result = [];
+    nums.sort((a, b) => a - b);
 
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
+            let left = i + 1;
+            let right = nums.length - 1;
+            const target = -nums[i];
+
+            while (left < right) {
+                const sum = nums[left] + nums[right];
+
+                if (sum === target) {
+                    result.push([nums[i], nums[left], nums[right]]);
+
+                    while (left < right && nums[left] === nums[left + 1]) left++;
+                    while (left < right && nums[right] === nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+    }
+
+    return result;
+};
