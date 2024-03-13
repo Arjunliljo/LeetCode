9985fe20b7b443d496c8ffc86e69118b03650e22
Arjun cv <arjun7180@gmail.com>
2024-03-13 19:17:36 +0530

    Easy -- String

--- Problems.js
+++ Problems.js
@@ -1493,3 +1493,20 @@ var reformatDate = function (date) {
     return date[0] + '-' + month + '-' + currDate;
 
 };
+
+//2278. Percentage of Letter in String
+var percentageLetter = function (s, letter) {
+
+    if (!s.includes(letter)) return 0;
+
+    let count = 0;
+
+    for (let i = 0; i < s.length; i++) {
+
+        if (s[i] === letter) {
+            count++;
+        }
+    }
+
+    return Math.floor((count / s.length) * 100);
+};
