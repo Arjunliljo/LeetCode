94bcac31583fc24350c1b1cb11300f0ae13dbe6d
Arjun cv <arjun7180@gmail.com>
2024-03-31 23:19:54 +0530

    Easy -- String

--- Problems.js
+++ Problems.js
@@ -1801,3 +1801,13 @@ var maxIceCream = function (costs, coins) {
 
     return i;
 };
+
+//1903. Largest Odd Number in String
+function largestOddNumber(num) {
+    for (let i = num.length - 1; i >= 0; i--) {
+        if (parseInt(num[i]) % 2 !== 0) {
+            return num.substring(0, i + 1);
+        }
+    }
+    return "";
+}
